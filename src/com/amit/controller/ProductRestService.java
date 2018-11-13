e87a91f79857528c1c088ac29ab2db2ed9d313d5
AMIT <amit>
2018-11-13 09:13:03 +0530

    JAX-RS 2.1 jar addedd

--- src/com/amit/controller/ProductRestService.java
+++ src/com/amit/controller/ProductRestService.java
@@ -1,5 +1,14 @@
 package com.amit.controller;
 
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 public class ProductRestService {
 
 }
